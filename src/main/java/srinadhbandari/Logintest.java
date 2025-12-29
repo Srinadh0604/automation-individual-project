7ff9bfd4c1b9a960948b745cb0c5f7b7455223d6
Srinadh0604 <Srinadh0604@gmail.com>
2025-12-29 22:40:28 +0530

    Initial project setup with Selenium configuration

---
+++ src/main/java/srinadhbandari/Logintest.java
@@ -0,0 +1,22 @@
+package srinadhbandari;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Logintest {
+    public static void main(String[] args) {
+    	
+        WebDriverManager.chromedriver().setup();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
